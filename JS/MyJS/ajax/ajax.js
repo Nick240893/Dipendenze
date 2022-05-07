927583b010c4fbd55dcefc2e9408923e30553d1a
Nick240893 <99483483+Nick240893@users.noreply.github.com>
2022-05-07 18:20:58 +0200

    Add files via upload

---
+++ JS/MyJS/ajax/ajax.js
@@ -0,0 +1,26 @@
+//AJAX
+function ajax(type,token,link,data) {
+ ['successAjax', 'errorAjax'].forEach(function(key) {local("del",key)});
+ $.ajax({
+  async: false,
+  type: type,
+  url: link,
+  headers: {"Authorization": "Bearer " + token},
+  data: data,
+  success: function(response){session("set","successAjax", JSON.stringify(response))},
+  error: function(response){session("set","errorAjax", JSON.stringify(response))},
+ });
+};
+
+//ajaxOnlyGET
+function ajaxOnlyGET(link) {
+ ['successAjax', 'errorAjax'].forEach(function(key) {local("del",key)});
+ $.ajax({
+  async: false,
+  type: 'GET',
+  url: link,
+  data: data,
+  success: function(response){session("set","successAjax", JSON.stringify(response))},
+  error: function(response){session("set","errorAjax", JSON.stringify(response))},
+ });
+};
